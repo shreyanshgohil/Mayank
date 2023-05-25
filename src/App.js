403cb4cfb6e446e37b70931534b9e8e0a8e0c6a4
shreyanshgohil <shreyanshgohil2510@gmail.com>
2023-05-25 21:54:05 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home, Login, Register } from './pages';
+import { Header } from './components/Global';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
     </Routes>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Toaster position="top-right" reverseOrder={false} />
     <BrowserRouter>
       <App />
     </BrowserRouter>
